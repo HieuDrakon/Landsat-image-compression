845f3b7f2d062f334743486f24563440e169bd11
Hiếu <124875874+HieuDrakon@users.noreply.github.com>
2024-10-25 15:40:55 +0700

    Update main.py

--- compression/main.py
+++ compression/main.py
@@ -1,84 +1,131 @@
-import tkinter as tk
+import os
+import time
+import io
+from PIL import Image
+import psycopg2
 from tkinter import messagebox
-from tkinter import filedialog
-from compress import compress_and_store_image
+import matplotlib.pyplot as plt
 from connect import con
-from uncompress import un
 
-def main_menu(id):
+def compress_and_store_image(image_path):  
     conn, cursor = con()
-    if id == 1:
-        image_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg;*.jpeg;*.png;*.tiff;*.tif")])
-        if image_path:  # Kiểm tra xem người dùng đã chọn file chưa
-            compress_and_store_image(image_path)
-        else:
-            root.mainloop()      
+    # Tạo bảng compressed_images nếu chưa có
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS compressed_images (
+        id SERIAL PRIMARY KEY,
+        name TEXT,
+        data BYTEA
+    )
+    """)
+    # Lấy tên tệp
+    image_name = os.path.basename(image_path)
+    # Lấy dung lượng ảnh trước khi nén
+    original_size = os.path.getsize(image_path)
+    
+    # Khởi tạo các biến lưu trữ thông tin
+    img_byte_arr_jpeg = None
+    img_byte_arr_lzw = None
+    img_byte_arr_png = None
+    compressed_size = []
+    compression_time = []
+    data = []
+    name = []
 
-    if id == 2:
-        menu.pack_forget()
-        frame.pack_forget()
-        frame.pack(padx=10,pady=10)
-        # Chọn dữ liệu trong database 
-        cursor.execute("SELECT id, name FROM compressed_images")
-        results = cursor.fetchall()  # Lấy tất cả các dòng kết quả
-        if not results:
-            messagebox.showinfo("Thông báo", "Data name is empty")
-        else:
-            # Xóa các checkbox cũ
-            for widget in frame.winfo_children():
-                widget.destroy()
+    try:
+        with Image.open(image_path) as img:
+            # Nén ảnh
+            # JPEG 2000
+            start_time_1 = time.time()
+            img_byte_arr_jpeg = io.BytesIO()
+            new_image_name_jp2 = image_name.rsplit('.', 1)[0] + '.jp2'
+            img.save(img_byte_arr_jpeg, format='JPEG2000')
+            img_byte_arr_jpeg = img_byte_arr_jpeg.getvalue()
+            end_time_1 = time.time()
 
-            # Tạo một danh sách để lưu trạng thái checkbox
-            checkboxes = []
+            # LZW
+            start_time_2 = time.time()
+            img_byte_arr_lzw = io.BytesIO()           
+            new_image_name_lzw = image_name.rsplit('.', 1)[0] + '.tiff'
+            img.save(img_byte_arr_lzw, format='TIFF', compression='tiff_lzw')
+            img_byte_arr_lzw = img_byte_arr_lzw.getvalue()
+            end_time_2 = time.time()
 
-            # Tạo checkbox cho mỗi tên file
-            for row in results:
-                var = tk.IntVar()  # Biến lưu trạng thái cho checkbox
-                checkbox = tk.Checkbutton(frame, text=row[1], variable=var)
-                checkbox.pack(anchor='w')  # Đặt checkbox vào frame
-                checkboxes.append((var, row[0], row[1]))  # Lưu trạng thái và tên file
+            # PNG
+            start_time_3 = time.time()
+            img_byte_arr_png = io.BytesIO()
+            new_image_name_png = image_name.rsplit('.', 1)[0] + '.png'
+            img.save(img_byte_arr_png, format='PNG', quality_mode='dB', quality_layers=[20])
+            img_byte_arr_png = img_byte_arr_png.getvalue()
+            end_time_3 = time.time()
 
-            # Thêm một nút để xử lý dữ liệu đã chọn
-            process_button = tk.Button(frame, text="Selected", command=lambda: un(checkboxes))
-            process_button.pack(pady=10)
-
-            # Nút trở về
-            return_button = tk.Button(frame, text="return", command=lambda: show_main_menu())
-            return_button.pack(pady=10)
-
-    if id == 3:
+    except Exception as e:
+        messagebox.showinfo("ERROR", f"Lỗi khi xử lý ảnh {image_name}: {e}")
         cursor.close()
         conn.close()
-        root.destroy()
+        return
 
-def show_main_menu():
-    frame.pack_forget()
-    menu.pack_forget() # Hiện lại menu chính
-    menu.pack(padx=100,pady=10)
-# Tạo giao diện chính
-root = tk.Tk()
-root.title("Image Compression Tool")
+    # Kiểm tra xem các biến có giá trị không trước khi tiếp tục
+    if img_byte_arr_jpeg is None or img_byte_arr_lzw is None or img_byte_arr_png is None:
+        messagebox.showinfo("ERROR", "Không thể nén ảnh. Vui lòng kiểm tra định dạng ảnh.")
+        cursor.close()
+        conn.close()
+        return
 
-# Tạo menu chọn
-menu = tk.Frame(root)
-menu.pack(padx=100,pady=10)
+    # Lấy dung lượng sau khi nén      
+    compressed_size = [len(img_byte_arr_jpeg), len(img_byte_arr_lzw), len(img_byte_arr_png)]
+    
+    # Tính thời gian nén
+    compression_time = [end_time_1 - start_time_1, end_time_2 - start_time_2, end_time_3 - start_time_3]
+    
+    # Lưu ảnh nén vào database
+    data = [img_byte_arr_jpeg, img_byte_arr_lzw, img_byte_arr_png]
+    name = [new_image_name_jp2, new_image_name_lzw, new_image_name_png]
+    for x, y in zip(name, data):
+        cursor.execute("""
+        INSERT INTO compressed_images (name, data)
+        VALUES (%s, %s)        
+        """, (x, psycopg2.Binary(y)))    
+    conn.commit()
 
- # Tạo một frame để chứa các checkbox
-frame = tk.Frame(root)
-frame.pack_forget()
-frame.pack(padx=10,pady=10)
+    # In thông tin        
+    print(f"Image name before compression: {image_name}\n",
+            f"Original Size: {original_size / (1024 ** 2):.2f} MB\n"                           
+            f"JPEG 2000\n"
+            f"Image name after compression: {new_image_name_jp2}\n"
+            f"Compressed Size: {compressed_size[0] / (1024 ** 2):.2f} MB\n"
+            f"Compression Time : {compression_time[0]:.2f} seconds\n"  
+            f"LZW\n" 
+            f"Image name after compression: {new_image_name_lzw}\n"                     
+            f"Compressed Size 2: {compressed_size[1] / (1024 ** 2):.2f} MB\n"
+            f"Compression Time 2: {compression_time[1]:.2f} seconds\n"
+            f"PNG\n"
+            f"Image name after compression: {new_image_name_png}\n"
+            f"Compressed Size 3: {compressed_size[2] / (1024 ** 2):.2f} MB\n"
+            f"Compression Time 3: {compression_time[2]:.2f} seconds")
+    
+    # Vẽ biểu đồ so sánh dung lượng nén với dung lượng ban đầu
+    compression_methods = ['JPEG 2000', 'LZW', 'PNG']
+    compressed_size_mb = [size / (1024 ** 2) for size in compressed_size]  # Dung lượng nén (MB)
+    original_size_mb = original_size / (1024 ** 2)  # Dung lượng ban đầu (MB)
 
-# Tạo nút để tải hình ảnh
-Select_button = tk.Button(menu, text="Select Image", command=lambda: main_menu(1))
-Select_button.pack(side=tk.TOP, fill=tk.X, padx=10,pady=10)
+    plt.figure(figsize=(10, 12))  # Tạo không gian cho cả hai biểu đồ
 
-# Nút để hiển thị tên ảnh đã nén
-decompressed_button = tk.Button(menu, text="Decompressed Image", command=lambda: main_menu(2))
-decompressed_button.pack(side=tk.TOP, fill=tk.X,padx=10,pady=10)
+    # Biểu đồ dung lượng nén so với dung lượng ban đầu
+    plt.subplot(2, 1, 1)  # Tạo biểu đồ trên cùng
+    plt.bar(compression_methods, compressed_size_mb, color=['blue', 'green', 'orange'])
+    plt.axhline(y=original_size_mb, color='red', linestyle='-', label='Original Size (MB)')
+    plt.title('Comparison of Compressed Sizes and Original Size')
+    plt.ylabel('Size (MB)')   
+    plt.legend()
 
-# Nút thoát chương trình
-close_button = tk.Button(menu, text="Exit", command=lambda: main_menu(3))
-close_button.pack(side=tk.TOP, fill=tk.X,padx=10,pady=10)
+    # Biểu đồ thời gian nén
+    plt.subplot(2, 1, 2)  # Tạo biểu đồ phía dưới
+    plt.bar(compression_methods, compression_time, color=['blue', 'green', 'orange'])
+    plt.title('Compression Time Comparison')
+    plt.ylabel('Time (seconds)')
 
-root.pack_propagate(True)
-root.mainloop()
+    # Hiển thị biểu đồ
+    plt.tight_layout()
+    plt.show()
+
+   
